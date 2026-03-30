c50b13590836d07d35e24abb0e599b0215ad6af3
jnllyng <jueunyang00@gmail.com>
2026-03-29 22:26:58 -0500

    Add Bootstrap styling with bootstrap

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,9 @@
 class Page < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :page_type, presence: true, inclusion: { in: [ "about", "contact" ] }
+
+  def self.ransackable_attributes(auth_object = nil)
+    [ "content", "created_at", "id", "page_type", "title", "updated_at" ]
+  end
 end
